8d89c1f233bcd199bfe1cff1de338e5234beecf8
Damien Legg <leggdamien@gmail.com>
2023-06-20 17:44:58 -0500

    completed searchHandler() function

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const input = document.querySelector('#fruit');
-const inputValue = input.value.toLowerCase();
 const suggestions = document.querySelector('.suggestions ul');
 
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
@@ -7,21 +6,31 @@ const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackb
 
 function search(str) { 
 	let results = [];
-
-	fruit.forEach((str) => {
-		if(str.substring(0, inputValue.length) === inputValue) {
-			results.push(str);
+	str = str.toLowerCase();
+	
+	//loop comes from codehim.com
+	for (let i = 0; i < fruit.length; i++) {
+		//loops through fruit array to see if str matches any strings
+		if (fruit[i].toLowerCase().indexOf(str) > -1) {
+			//if str matches any of the strings then the string is pushed to results
+			results.push(fruit[i]);
 		}
-	})
+	}
 	
 	return results;
 }
 
 function searchHandler(e) {
-	// TODO
+	let inputVal = e.target.value;
+	let results = [];
+	if (inputVal.length !== 0) {
+		results = search(inputVal);
+	}
+	showSuggestions(results,inputVal);
 }
 
 function showSuggestions(results, inputVal) {
+	
 }
 
 function useSuggestion(e) {
